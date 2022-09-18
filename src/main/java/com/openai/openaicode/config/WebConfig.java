e5c0a5c1e3140d92243f21b590cd3115691d40cd
soham <soham.hasabnis.0998>
2022-09-19 00:09:55 +0530

    added system

--- src/main/java/com/openai/openaicode/config/WebConfig.java
+++ src/main/java/com/openai/openaicode/config/WebConfig.java
@@ -2,11 +2,10 @@ package com.openai.openaicode.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
