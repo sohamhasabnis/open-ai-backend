69218a92eb270b721784ea922a73477e310b2369
soham <soham.hasabnis.0998>
2022-09-19 09:58:42 +0530

    added system

--- src/main/java/com/openai/openaicode/repository/OpenAIRepoImpl.java
+++ src/main/java/com/openai/openaicode/repository/OpenAIRepoImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.Base64;
 
 @Repository
 public class OpenAIRepoImpl implements OpenAI{
@@ -32,7 +33,7 @@ public class OpenAIRepoImpl implements OpenAI{
     public SchemaResponse getResponse(Schema schema) {
         String url = "https://api.openai.com/v1/completions";
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setBearerAuth(authToken);
+        httpHeaders.setBearerAuth(new String(Base64.getDecoder().decode(authToken)));
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         ObjectMapper objectMapper = new ObjectMapper();
         String a = null;
